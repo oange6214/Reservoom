332981b5a38f8235b2ad52d2976b9535026cb881
jed <oange6214@gmail.com>
2022-07-24 14:24:41 +0800

    Models - WPF MVVM TUTORIAL #1

--- Reservoom/App.xaml.cs
+++ Reservoom/App.xaml.cs
@@ -1,17 +1,45 @@
-﻿using System;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Reservoom
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("SingletonSean Suites");
+
+            try
+            {
+                hotel.MakeReservation(
+                    new Reservation(
+                        new RoomID(1, 3),
+                        "SingletonSean",
+                        new DateTime(2000, 1, 1),
+                        new DateTime(2000, 1, 2)
+                        )
+                    );
+
+                hotel.MakeReservation(
+                    new Reservation(
+                        new RoomID(1, 2),
+                        "SingletonSean",
+                        new DateTime(2000, 1, 3),
+                        new DateTime(2000, 1, 4)
+                        )
+                    );
+            }
+            catch (ReservationConflictException ex)
+            {
+                throw;
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("SingletonSean");
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Reservoom/Models/Hotel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class Hotel
+    {
+        private readonly ReservationBook _reservationBook;
+        public string Name { get; }
+
+        public Hotel(string name)
+        {
+            Name = name;
+            _reservationBook = new ReservationBook();
+        }
+
+        /// <summary>
+        /// Get the reservations for a user.
+        /// </summary>
+        /// <param name="userName">The userName of the user.</param>
+        /// <returns>The reservations for the user.</returns>
+        public IEnumerable<Reservation> GetReservationsForUser(string userName)
+        {
+            return _reservationBook.GetReservationsForUser(userName);
+        }
+
+        /// <summary>
+        /// Make a reservation
+        /// </summary>
+        /// <param name="reservation">The incoming reservation.</param>
+        /// <exception cref="ReservationClict"
+        public void MakeReservation(Reservation reservation)
+        {
+            _reservationBook.AddReservation(reservation);
+        }
+    }
+}
