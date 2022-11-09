8380a772d1bee8b96a28c6f7d06b27fe45ca6e2f
oange6214 <oange6214@gmail.com>
2022-11-09 15:31:51 +0800

    Update

--- Reservoom/obj/Debug/net6.0-windows/App.g.i.cs
+++ Reservoom/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,13 +47,13 @@ namespace Reservoom {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Reservoom;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Reservoom;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -67,7 +67,7 @@ namespace Reservoom {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
         public static void Main() {
             Reservoom.App app = new Reservoom.App();
             app.InitializeComponent();
